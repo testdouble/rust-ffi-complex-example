0d13135bcafddfef228df25fd6a08afe41cb943e
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2018-04-20 14:42:02 -0700

    Add server behaviour.

--- node/server.js
+++ node/server.js
@@ -6,18 +6,37 @@ const CONNECT_EVENT_ID = 1
 const DISCONNECT_EVENT_ID = 2
 const UPDATE_EVENT_ID = 3
 
+const knownClients = {}
+
+function addKnownClient(id, rinfo) {
+  knownClients[id] = { rinfo }
+}
+
+function removeKnownClient(id) {
+  delete knownClients[id]
+}
+
+function broadcastUpdateToKnownClients(buffer) {
+  Reflect.ownKeys(knownClients)
+    .map(clientId => knownClients[clientId])
+    .forEach(rinfo => socket.send(buffer, rinfo.port, rinfo.address))
+}
+
 socket.on('message', function (message, rinfo) {
   var eventId = "PING"
 
   switch (message[0]) {
     case CONNECT_EVENT_ID:
       eventId = "CONNECT"
+      addKnownClient(message.readUInt32BE(1))
       break
     case DISCONNECT_EVENT_ID:
       eventId = "DISCONNECT"
+      removeKnownClient(message.readUInt32BE(1))
       break
     case UPDATE_EVENT_ID:
       eventId = "UPDATE"
+      broadcastUpdateToKnownClients(message)
       break
   }
 
